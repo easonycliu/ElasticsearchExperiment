9f8468dc77cd55a0b5fd618818b510c1b5175685
liuyc1515 <liuyicheng151@sjtu.edu.cn>
2024-04-10 15:30:33 +0000

    partial finish c3 settings

--- scripts/multiclient_nested_aggs.sh
+++ scripts/multiclient_nested_aggs.sh
@@ -4,12 +4,10 @@ set -m
 client_num=$1
 exp_duration=60
 burst_time=10
-interfere=12
 
-search_indices=test10,test11,test12,test13,test14,test15,test16,test17,test18,test19,test20,test21,test22,test23,test24,test25,test26,test27,test28,test29
-interfere_indices=test50
-for i in {51..99}; do
-	interfere_indices=$interfere_indices,test$i
+search_indices=test10
+for i in $(seq 11 1 80); do
+    search_indices=$search_indices,test$i
 done
 
 file_name=tmp_$(date +%Y%m%d%H%M%S)
@@ -38,13 +36,6 @@ for j in $(seq 1 1 $exp_duration); do
             echo $j
             start_us=$(date +"%s%6N")
             curl -X GET -H "Content-Type:application/json" --data-binary @${PWD}/query/nest_aggs.json http://localhost:9200/$search_indices/_search?pretty | tail -n 20 &
-            end_us=$(date +"%s%6N")
-            echo $(( end_us - start_us )) >> ${baseline_info[0]}
-        fi
-        if [[ "$j" == "$interfere" ]]; then
-            echo $j
-            start_us=$(date +"%s%6N")
-            curl -X GET -H "Content-Type:application/json" --data-binary @query/boolean_search_interfere.json http://localhost:9200/$interfere_indices/_search?pretty | tail -n 5 &
             end_us=$(date +"%s%6N")
             echo $(( end_us - start_us )) >> ${baseline_info[0]}
         fi
